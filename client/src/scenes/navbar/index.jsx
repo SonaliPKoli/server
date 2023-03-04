73f5628c2926febe70960bee58b0b6866b3e5582
SonaliPKoli <sonalikoli2002@gmail.com>
2023-03-04 21:39:39 +0530

    navbar changes

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -24,31 +24,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { setMode, setLogout } from "../../state";
 import { useNavigate } from "react-router-dom";
 import FlexBetween from "../../components/FlexBetween";
-
 const Navbar = () => {
-  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const [isMoblieMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px");
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
-
   const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
-
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
-      
       <FlexBetween gap="1.75rem">
         <Typography
           fontWeight="bold"
-          fontSize="clamp(1rem, 2rem, 2.25rem)"
+          fontSize="clamp(1rem,2rem,2.25rem)"
           color="primary"
           onClick={() => navigate("/home")}
           sx={{
@@ -74,55 +69,42 @@ const Navbar = () => {
           </FlexBetween>
         )}
       </FlexBetween>
-
-      {/* DESKTOP NAV */}
+      {/* Desktop nav */}
       {isNonMobileScreens ? (
         <FlexBetween gap="2rem">
           <IconButton onClick={() => dispatch(setMode())}>
             {theme.palette.mode === "dark" ? (
-              <DarkMode sx={{ fontSize: "25px" }} />
+              Box(<DarkMode sx={{ fontSize: "25px" }} />)
             ) : (
               <LightMode sx={{ color: dark, fontSize: "25px" }} />
             )}
           </IconButton>
-          <Message sx={{ fontSize: "25px" }} />
+          <Message sx={{  fontSize: "25px" }} />
           <Notifications sx={{ fontSize: "25px" }} />
           <Help sx={{ fontSize: "25px" }} />
           <FormControl variant="standard" value={fullName}>
-            <Select
-              value={fullName}
-              sx={{
-                backgroundColor: neutralLight,
-                width: "150px",
-                borderRadius: "0.25rem",
-                p: "0.25rem 1rem",
-                "& .MuiSvgIcon-root": {
-                  pr: "0.25rem",
-                  width: "3rem",
-                },
-                "& .MuiSelect-select:focus": {
-                  backgroundColor: neutralLight,
-                },
-              }}
-              input={<InputBase />}
-            >
-              <MenuItem value={fullName}>
+            <Select value={fullName}
+            sx={{
+                backgroundColor:neutralLight,
+                width:"150px",
+                borderRadius:"0.25rem",
+                p:"0.25rem 1rem",
+                "& .MuiSvgIcon-root":{
+                   backgroundColor:neutralLight
+                }
+            }}
+            > <MenuItem value={fullName}>
                 <Typography>{fullName}</Typography>
               </MenuItem>
-              <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
-            </Select>
+              <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem></Select>
           </FormControl>
         </FlexBetween>
       ) : (
-        <IconButton
-          onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
-        >
-          <Menu />
+        <IconButton onClick={() => setIsMobileMenuToggled(!isMoblieMenuToggled)}>
+            <Menu/>
         </IconButton>
       )}
-
-      {/* MOBILE NAV */}
-      {!isNonMobileScreens && isMobileMenuToggled && (
+      {!isNonMobileScreens && isMoblieMenuToggled && (
         <Box
           position="fixed"
           right="0"
@@ -136,13 +118,11 @@ const Navbar = () => {
           {/* CLOSE ICON */}
           <Box display="flex" justifyContent="flex-end" p="1rem">
             <IconButton
-              onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+              onClick={() => setIsMobileMenuToggled(!isMoblieMenuToggled)}
             >
               <Close />
             </IconButton>
           </Box>
-
-          {/* MENU ITEMS */}
           <FlexBetween
             display="flex"
             flexDirection="column"
@@ -195,5 +175,4 @@ const Navbar = () => {
     </FlexBetween>
   );
 };
-
 export default Navbar;
